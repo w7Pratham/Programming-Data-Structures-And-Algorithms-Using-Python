6da69b51f0ffe1759a5efaddfc87e06245886669
Prathamesh Shingne <prathamshingne@gmail.com>
2022-09-27 17:47:13 +0530

    Create Using_temp_array.py

---
+++ Array/Array_rotation/Using_temp_array.py
@@ -0,0 +1,14 @@
+"""
+Approach: •First store the elements from d to n-1 in a temp array.
+•later store the d elements from original array into tmp array.
+•Copy elements from tmp array into the orginal array.
+"""
+
+def rotate(ls, d):
+    tmp = []
+    tmp.extend(ls[d:] + ls[:d])
+    return tmp
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5]
+    print(rotate(arr, 2))
